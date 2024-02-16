505f5878aff5325422e2e0cc5c2b3e8eeec8001d
Nikolay Sedov <nicksedov@yandex.ru>
2024-02-16 20:58:05 +0300

    Optimized gorm select query for events

--- pkg/model/init.go
+++ pkg/model/init.go
@@ -6,7 +6,6 @@ import (
 	"github.com/nicksedov/gptbot/pkg/settings"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 )
 
 var sqliteDb *gorm.DB
@@ -27,7 +26,7 @@ func GetEvents() ([]SingleEvent, error) {
 		return nil, err
 	}
 	var events []SingleEvent
-	db.Preload("EventPromptParams.PromptParam").Joins("Chat").Preload(clause.Associations).Find(&events)
+	db.Preload("EventPromptParams.PromptParam").Joins("Prompt").Joins("Chat").Find(&events)
 	return events, nil
 }
 
@@ -40,37 +39,4 @@ func GetAll[T any]() ([]T, error) {
 	var items []T
 	db.Find(&items)
 	return items, nil
-}
-
-func GetChats() ([]TelegramChat, error) {
-	db, err := initDb()
-	if err != nil {
-		log.Fatal("failed to connect database")
-		return nil, err
-	}
-	var chats []TelegramChat
-	db.Find(&chats)
-	return chats, nil
-}
-
-func GetPrompts() ([]Prompt, error) {
-	db, err := initDb()
-	if err != nil {
-		log.Fatal("failed to connect database")
-		return nil, err
-	}
-	var prompts []Prompt
-	db.Find(&prompts)
-	return prompts, nil
-}
-
-func GetPromptParams() ([]PromptParam, error) {
-	db, err := initDb()
-	if err != nil {
-		log.Fatal("failed to connect database")
-		return nil, err
-	}
-	var promptParams []PromptParam
-	db.Find(&promptParams)
-	return promptParams, nil
 }
